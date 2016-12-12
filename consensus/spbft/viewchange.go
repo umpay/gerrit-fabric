3826e0a525a4b56418c14a3ea00ec3eb02fa689d
liaoxj <273789788@qq.com>
2016-12-12 10:06:39 +0800

    fix viewchange bug

--- consensus/spbft/viewchange.go
+++ consensus/spbft/viewchange.go
@@ -16,12 +16,11 @@ limitations under the License.
 
 package spbft
 
-
 import (
 	"encoding/base64"
 	"fmt"
 	"reflect"
-
+	// "sync"
 	"github.com/hyperledger/fabric/consensus/util/events"
 )
 
@@ -29,17 +28,11 @@ import (
 type viewChangeQuorumEvent struct{}
 
 func (instance *spbftCore) correctViewChange(vc *ViewChange) bool {
-	// for _, p := range append(vc.Pset, vc.Qset...) {
-	// 	if !(p.View < vc.View && p.SequenceNumber > vc.H && p.SequenceNumber <= vc.H+instance.L) {
-	// 		logger.Debugf("Replica %d invalid p entry in view-change: vc(v:%d h:%d) p(v:%d n:%d)",
-	// 			instance.id, vc.View, vc.H, p.View, p.SequenceNumber)
-	// 		return false
-	// 	}
-	// }
-
 	for _, q := range vc.Qset {
 		if !(q.View < vc.View && q.SequenceNumber > vc.H && q.SequenceNumber <= vc.H+instance.L) {
 			logger.Debugf("Replica %d invalid q entry in view-change: vc(v:%d h:%d) q(v:%d n:%d)",
+				instance.id, vc.View, vc.H, q.View, q.SequenceNumber)
+			logger.Infof("Replica %d invalid q entry in view-change: vc(v:%d h:%d) q(v:%d n:%d)",
 				instance.id, vc.View, vc.H, q.View, q.SequenceNumber)
 			return false
 		}
@@ -49,48 +42,14 @@ func (instance *spbftCore) correctViewChange(vc *ViewChange) bool {
 		if !(c.SequenceNumber >= vc.H && c.SequenceNumber <= vc.H+instance.L) {
 			logger.Debugf("Replica %d invalid c entry in view-change: vc(v:%d h:%d) c(n:%d)",
 				instance.id, vc.View, vc.H, c.SequenceNumber)
+			logger.Infof("Replica %d invalid c entry in view-change: vc(v:%d h:%d) c(n:%d)",
+				instance.id, vc.View, vc.H, c.SequenceNumber)
 			return false
 		}
 	}
 
 	return true
 }
-
-// func (instance *pbftCore) calcPSet() map[uint64]*ViewChange_PQ {
-// 	pset := make(map[uint64]*ViewChange_PQ)
-
-// 	for n, p := range instance.pset {
-// 		pset[n] = p
-// 	}
-
-// 	// P set: requests that have prepared here
-// 	//
-// 	// "<n,d,v> has a prepared certificate, and no request
-// 	// prepared in a later view with the same number"
-
-// 	for idx, cert := range instance.certStore {
-// 		if cert.prePrepare == nil {
-// 			continue
-// 		}
-
-// 		digest := cert.digest
-// 		if !instance.prepared(digest, idx.v, idx.n) {
-// 			continue
-// 		}
-
-// 		if p, ok := pset[idx.n]; ok && p.View > idx.v {
-// 			continue
-// 		}
-
-// 		pset[idx.n] = &ViewChange_PQ{
-// 			SequenceNumber: idx.n,
-// 			BatchDigest:    digest,
-// 			View:           idx.v,
-// 		}
-// 	}
-
-// 	return pset
-// }
 
 func (instance *spbftCore) calcQSet() map[qidx]*ViewChange_PQ {
 	qset := make(map[qidx]*ViewChange_PQ)
@@ -137,12 +96,12 @@ func (instance *spbftCore) sendViewChange() events.Event {
 	instance.view++
 	instance.activeView = false
 
-	//instance.pset = instance.calcPSet()
 	instance.qset = instance.calcQSet()
 
 	// clear old messages
 	for idx := range instance.certStore {
 		if idx.v < instance.view {
+			logger.Infof("--sendViewChange-del certStore-id:%d curV:%d  n/v:%d/%d hash:%s", instance.id, instance.view, idx.n, idx.v, instance.certStore[idx].digest)
 			delete(instance.certStore, idx)
 		}
 	}
@@ -165,13 +124,6 @@ func (instance *spbftCore) sendViewChange() events.Event {
 		})
 	}
 
-	// for _, p := range instance.pset {
-	// 	if p.SequenceNumber < instance.h {
-	// 		logger.Errorf("BUG! Replica %d should not have anything in our pset less than h, found %+v", instance.id, p)
-	// 	}
-	// 	vc.Pset = append(vc.Pset, p)
-	// }
-
 	for _, q := range instance.qset {
 		if q.SequenceNumber < instance.h {
 			logger.Errorf("BUG! Replica %d should not have anything in our qset less than h, found %+v", instance.id, q)
@@ -180,9 +132,6 @@ func (instance *spbftCore) sendViewChange() events.Event {
 	}
 
 	instance.sign(vc)
-
-	// logger.Infof("Replica %d sending view-change, v:%d, h:%d, |C|:%d, |P|:%d, |Q|:%d",
-	// 	instance.id, vc.View, vc.H, len(vc.Cset), len(vc.Pset), len(vc.Qset))
 
 	logger.Infof("Replica %d sending view-change, v:%d, h:%d, |C|:%d, |Q|:%d",
 		instance.id, vc.View, vc.H, len(vc.Cset), len(vc.Qset))
@@ -195,33 +144,36 @@ func (instance *spbftCore) sendViewChange() events.Event {
 }
 
 func (instance *spbftCore) recvViewChange(vc *ViewChange) events.Event {
-	// logger.Infof("Replica %d received view-change from replica %d, v:%d, h:%d, |C|:%d, |P|:%d, |Q|:%d",
-	// 	instance.id, vc.ReplicaId, vc.View, vc.H, len(vc.Cset), len(vc.Pset), len(vc.Qset))
-
 	logger.Infof("Replica %d received view-change from replica %d, v:%d, h:%d, |C|:%d, |Q|:%d",
 		instance.id, vc.ReplicaId, vc.View, vc.H, len(vc.Cset), len(vc.Qset))
+	maxExec := instance.lastExec
+   	if instance.currentExec != nil {
+    	maxExec = *instance.currentExec
+        logger.Infof("Replica %d update maxExec to %d", instance.id, maxExec)
+    }
 
 	if err := instance.verify(vc); err != nil {
-		logger.Warningf("Replica %d found incorrect signature in view-change message: %s", instance.id, err)
+		logger.Warningf("Replica %d found incorrect signature in view-change message: %s  for sender: %d", instance.id, err, vc.ReplicaId)
 		return nil
 	}
 
 	if vc.View < instance.view {
-		logger.Warningf("Replica %d found view-change message for old view", instance.id)
+		logger.Warningf("Replica %d found view-change message for old view for sender: %d", instance.id, vc.ReplicaId)
 		return nil
 	}
 
 	if !instance.correctViewChange(vc) {
-		logger.Warningf("Replica %d found view-change message incorrect", instance.id)
+		logger.Warningf("Replica %d found view-change message incorrect for sender: %d", instance.id, vc.ReplicaId)
 		return nil
 	}
 
 	if _, ok := instance.viewChangeStore[vcidx{vc.View, vc.ReplicaId}]; ok {
 		logger.Warningf("Replica %d already has a view change message for view %d from replica %d", instance.id, vc.View, vc.ReplicaId)
+	    instance.viewChangeSeqNo = maxExec/10*10 + 10
 		return nil
 	}
-
 	instance.viewChangeStore[vcidx{vc.View, vc.ReplicaId}] = vc
+	
 
 	// PBFT TOCS 4.5.1 Liveness: "if a replica receives a set of
 	// f+1 valid VIEW-CHANGE messages from other replicas for
@@ -257,8 +209,10 @@ func (instance *spbftCore) recvViewChange(vc *ViewChange) events.Event {
 		}
 	}
 	logger.Debugf("Replica %d now has %d view change requests for view %d", instance.id, quorum, instance.view)
+	logger.Infof("Replica %d now has %d view change requests for view %d", instance.id, quorum, instance.view)
 
 	if !instance.activeView && vc.View == instance.view && quorum >= instance.allCorrectReplicasQuorum() {
+		logger.Infof("node %d the maxExec for viewChange is %d, now view is  %d", instance.id, maxExec, instance.view)
 		instance.vcResendTimer.Stop()
 		instance.startTimer(instance.lastNewViewTimeout, "new view change")
 		instance.lastNewViewTimeout = 2 * instance.lastNewViewTimeout
@@ -268,27 +222,27 @@ func (instance *spbftCore) recvViewChange(vc *ViewChange) events.Event {
 	return nil
 }
 
-func (instance *spbftCore) showMsgList(funcName string,seqNo uint64,vset []*ViewChange,msgList map[uint64]string){
-	//logger.Errorf("---funcName:%s",funcName)
+func (instance *spbftCore) showMsgList(funcName string, seqNo uint64, vset []*ViewChange, msgList map[uint64]string) {
+	logger.Infof("---funcName:%s",funcName)
 	var res string
-	res = fmt.Sprintf("-------------vset------------------\n----funcName:%s  seqNum:%d vsetSize:%d \n",funcName,seqNo,len(vset))
-	for i,em :=range vset{
-		res += fmt.Sprintf("----index:%d  view:%d  h:%d ReplicaId:%d  CsetSize:%d  QsetSize:%d\n",i,em.View,em.H,em.ReplicaId,len(em.Cset),len(em.Qset))
-		for ic,vcc:= range em.Cset{
-			res += fmt.Sprintf("---RId:%d--%d--Cset--index:%d  seqNo:%d  id:%s\n",instance.id,i,ic,vcc.SequenceNumber,vcc.Id)
+	res = fmt.Sprintf("-------------vset------------------\n----funcName:%s  seqNum:%d vsetSize:%d \n", funcName, seqNo, len(vset))
+	for i, em := range vset {
+		res += fmt.Sprintf("----index:%d  view:%d  h:%d ReplicaId:%d  CsetSize:%d  QsetSize:%d\n", i, em.View, em.H, em.ReplicaId, len(em.Cset), len(em.Qset))
+		for ic, vcc := range em.Cset {
+			res += fmt.Sprintf("---RId:%d--%d--Cset--index:%d  seqNo:%d  id:%s\n", instance.id, i, ic, vcc.SequenceNumber, vcc.Id)
 		}
-		for ic,qcc:= range em.Qset{
-			res += fmt.Sprintf("---RId:%d--%d--Qset--index:%d  seqNo:%d view:%d Bhash:%s\n",instance.id,i,ic,qcc.SequenceNumber,qcc.View,qcc.BatchDigest)
+		for ic, qcc := range em.Qset {
+			res += fmt.Sprintf("---RId:%d--%d--Qset--index:%d  seqNo:%d view:%d Bhash:%s\n", instance.id, i, ic, qcc.SequenceNumber, qcc.View, qcc.BatchDigest)
 		}
 	}
-	res +=fmt.Sprintf("------------msgList-------  msgSize:%d\n",instance.id,len(msgList))
+	res += fmt.Sprintf("------------msgList-------  msgSize:%d\n", instance.id, len(msgList))
 	var index int
-	index =0
-	for k,v:= range msgList{
-		res += fmt.Sprintf("-----RId:%d  ID:%d key:%d hash:%s\n",instance.id,index,k,v)
-		index +=1
+	index = 0
+	for k, v := range msgList {
+		res += fmt.Sprintf("-----RId:%d  ID:%d key:%d hash:%s\n", instance.id, index, k, v)
+		index += 1
 	}
-//	logger.Testf("\n--------showMsgList------ReplicaId：%d--------\n%s\n",instance.id,res)
+		//logger.Infof("\n--------showMsgList------ReplicaId：%d--------\n%s\n",instance.id,res)
 }
 
 func (instance *spbftCore) sendNewView() events.Event {
@@ -307,7 +261,7 @@ func (instance *spbftCore) sendNewView() events.Event {
 	}
 
 	msgList := instance.assignSequenceNumbers(vset, cp.SequenceNumber)
-	instance.showMsgList("sendNewView",cp.SequenceNumber,vset,msgList)
+	instance.showMsgList("sendNewView", cp.SequenceNumber, vset, msgList)
 	if msgList == nil {
 		logger.Infof("Replica %d could not assign sequence numbers for new view", instance.id)
 		return nil
@@ -421,7 +375,7 @@ func (instance *spbftCore) processNewView() events.Event {
 	}
 
 	msgList := instance.assignSequenceNumbers(nv.Vset, cp.SequenceNumber)
-	instance.showMsgList("processNewView",cp.SequenceNumber,nv.Vset,msgList)
+	instance.showMsgList("processNewView", cp.SequenceNumber, nv.Vset, msgList)
 	if msgList == nil {
 		logger.Warningf("Replica %d could not assign sequence numbers: %+v",
 			instance.id, instance.viewChangeStore)
@@ -485,7 +439,7 @@ func (instance *spbftCore) processNewView() events.Event {
 	}
 
 	if len(instance.missingReqBatches) == 0 {
-		return instance.processNewView2(nv)
+		return instance.processNewView2(nv, cp)
 	} else if newReqBatchMissing {
 		instance.fetchRequestBatches()
 	}
@@ -493,7 +447,7 @@ func (instance *spbftCore) processNewView() events.Event {
 	return nil
 }
 
-func (instance *spbftCore) processNewView2(nv *NewView) events.Event {
+func (instance *spbftCore) processNewView2(nv *NewView, cp ViewChange_C) events.Event {
 	logger.Infof("Replica %d accepting new-view to view %d", instance.id, instance.view)
 
 	instance.stopTimer()
@@ -502,7 +456,21 @@ func (instance *spbftCore) processNewView2(nv *NewView) events.Event {
 	instance.activeView = true
 	delete(instance.newViewStore, instance.view-1)
 
+	if len(instance.chkpts) == 0 {
+		instance.chkpts[cp.SequenceNumber] = cp.Id
+		logger.Infof("Replica %d adding instance.chkpts for %d", instance.id, cp.SequenceNumber)
+		snapshotID, err := base64.StdEncoding.DecodeString(cp.Id)
+		if nil != err {
+			err = fmt.Errorf("Replica %d Decode  Checkpoint for seqNo %d failed", instance.id, cp.SequenceNumber)
+			logger.Error(err.Error())
+			return nil
+		}
+		instance.persistCheckpoint(cp.SequenceNumber, snapshotID)
+		logger.Infof("Replica %d persistCheckpoint for  seqNo %d", instance.id, cp.SequenceNumber)
+	}
+
 	instance.seqNo = instance.h
+	
 	for n, d := range nv.Xset {
 		if n <= instance.h {
 			continue
@@ -529,34 +497,32 @@ func (instance *spbftCore) processNewView2(nv *NewView) events.Event {
 	}
 
 	instance.updateViewChangeSeqNo()
+	logger.Infof("Replica %d successed update ViewChangeSeqNo", instance.id)
 
-	if instance.primary(instance.view) != instance.id {
-		for n, d := range nv.Xset {
-			commit := &Commit{
-				View:           instance.view,
-				SequenceNumber: n,
-				BatchDigest:    d,
-				ReplicaId:      instance.id,
-			}
-			// cert.sentCommit = true
-			// instance.recvCommit(commit)
-			// return instance.innerBroadcast(&Message{&Message_Commit{commit}})
-			if n > instance.h {
-				cert := instance.getCert(instance.view, n)
-				cert.sentCommit = true
-				instance.recvCommit(commit)
-			}
-			instance.innerBroadcast(&Message{&Message_Commit{commit}})
+	for n, d := range nv.Xset {
+		commit := &Commit{
+			View:           instance.view,
+			SequenceNumber: n,
+			BatchDigest:    d,
+			ReplicaId:      instance.id,
 		}
-	} else {
+
+		if n > instance.h {
+			cert := instance.getCert(instance.view, n)
+			cert.sentCommit = true
+			instance.recvCommit(commit)
+		}
+		instance.innerBroadcast(&Message{&Message_Commit{commit}})
+	}
+	if instance.primary(instance.view) == instance.id {
 		logger.Debugf("Replica %d is now primary, attempting to resubmit requests", instance.id)
 		instance.resubmitRequestBatches()
 	}
 
-	instance.startTimerIfOutstandingRequests()
+	//instance.startTimerIfOutstandingRequests()
 
 	logger.Debugf("Replica %d done cleaning view change artifacts, calling into consumer", instance.id)
-        instance.delReqBatch()
+	instance.delReqBatch()
 	return viewChangedEvent{}
 }
 
@@ -574,6 +540,7 @@ func (instance *spbftCore) selectInitialCheckpoint(vset []*ViewChange) (checkpoi
 		for _, c := range vc.Cset { // TODO, verify that we strip duplicate checkpoints from this set
 			checkpoints[*c] = append(checkpoints[*c], vc)
 			logger.Debugf("Replica %d appending checkpoint from replica %d with seqNo=%d, h=%d, and checkpoint digest %s", instance.id, vc.ReplicaId, vc.H, c.SequenceNumber, c.Id)
+
 		}
 	}
 
@@ -598,9 +565,11 @@ func (instance *spbftCore) selectInitialCheckpoint(vset []*ViewChange) (checkpoi
 		for _, vc := range vset {
 			if vc.H <= idx.SequenceNumber {
 				quorum++
+				logger.Infof("Replica %d find  checkpoint from replica %d  for quorum %", instance.id, vc.ReplicaId, quorum)
 			}
 		}
 
+		logger.Infof("Replica %d has %d quorum  for n:%d", instance.id, quorum, idx.SequenceNumber)
 		if quorum < instance.intersectionQuorum() {
 			logger.Debugf("Replica %d has no quorum for n:%d", instance.id, idx.SequenceNumber)
 			continue
@@ -620,95 +589,6 @@ func (instance *spbftCore) selectInitialCheckpoint(vset []*ViewChange) (checkpoi
 	return
 }
 
-// func (instance *pbftCore) assignSequenceNumbers(vset []*ViewChange, h uint64) (msgList map[uint64]string) {
-// 	msgList = make(map[uint64]string)
-
-// 	maxN := h + 1
-
-// 	// "for all n such that h < n <= h + L"
-// nLoop:
-// 	for n := h + 1; n <= h+instance.L; n++ {
-// 		// "∃m ∈ S..."
-// 		for _, m := range vset {
-// 			// "...with <n,d,v> ∈ m.P"
-// 			for _, em := range m.Pset {
-// 				quorum := 0
-// 				// "A1. ∃2f+1 messages m' ∈ S"
-// 			mpLoop:
-// 				for _, mp := range vset {
-// 					if mp.H >= n {
-// 						continue
-// 					}
-// 					// "∀<n,d',v'> ∈ m'.P"
-// 					for _, emp := range mp.Pset {
-// 						if n == emp.SequenceNumber && !(emp.View < em.View || (emp.View == em.View && emp.BatchDigest == em.BatchDigest)) {
-// 							continue mpLoop
-// 						}
-// 					}
-// 					quorum++
-// 				}
-
-// 				if quorum < instance.intersectionQuorum() {
-// 					continue
-// 				}
-
-// 				quorum = 0
-// 				// "A2. ∃f+1 messages m' ∈ S"
-// 				for _, mp := range vset {
-// 					// "∃<n,d',v'> ∈ m'.Q"
-// 					for _, emp := range mp.Qset {
-// 						if n == emp.SequenceNumber && emp.View >= em.View && emp.BatchDigest == em.BatchDigest {
-// 							quorum++
-// 						}
-// 					}
-// 				}
-
-// 				if quorum < instance.f+1 {
-// 					continue
-// 				}
-
-// 				// "then select the request with digest d for number n"
-// 				msgList[n] = em.BatchDigest
-// 				maxN = n
-
-// 				continue nLoop
-// 			}
-// 		}
-
-// 		quorum := 0
-// 		// "else if ∃2f+1 messages m ∈ S"
-// 	nullLoop:
-// 		for _, m := range vset {
-// 			// "m.P has no entry"
-// 			for _, em := range m.Pset {
-// 				if em.SequenceNumber == n {
-// 					continue nullLoop
-// 				}
-// 			}
-// 			quorum++
-// 		}
-
-// 		if quorum >= instance.intersectionQuorum() {
-// 			// "then select the null request for number n"
-// 			msgList[n] = ""
-
-// 			continue nLoop
-// 		}
-
-// 		logger.Warningf("Replica %d could not assign value to contents of seqNo %d, found only %d missing P entries", instance.id, n, quorum)
-// 		return nil
-// 	}
-
-// 	// prune top null requests
-// 	for n, msg := range msgList {
-// 		if n > maxN && msg == "" {
-// 			delete(msgList, n)
-// 		}
-// 	}
-
-// 	return
-// }
-
 func (instance *spbftCore) assignSequenceNumbers(vset []*ViewChange, h uint64) (msgList map[uint64]string) {
 	msgList = make(map[uint64]string)
 
@@ -718,35 +598,11 @@ func (instance *spbftCore) assignSequenceNumbers(vset []*ViewChange, h uint64) (
 nLoop:
 	for n := h + 1; n <= h+instance.L; n++ {
 		// "∃m ∈ S..."
-		// 遍历vset
 		for _, m := range vset {
 			// "...with <n,d,v> ∈ m.P"
-			//遍历每一个ViewChange中的Qset
 			for _, em := range m.Qset {
 				quorum := 0
-				// "A1. ∃2f+1 messages m' ∈ S"
-				// 存在2f+1个消息
-				// mpLoop:
-				// 	for _, mp := range vset {
-				// 		if mp.H >= n {
-				// 			continue
-				// 		}
-				// 		// "∀<n,d',v'> ∈ m'.P"
-				// 		// 所有
-				// 		for _, emp := range mp.Pset {
-				// 			if n == emp.SequenceNumber && !(emp.View < em.View || (emp.View == em.View && emp.BatchDigest == em.BatchDigest)) {
-				// 				continue mpLoop
-				// 			}
-				// 		}
-				// 		quorum++
-				// 	}
-
-				// 	if quorum < instance.intersectionQuorum() {
-				// 		continue
-				// 	}
-
-				// 	quorum = 0
-				// "A2. ∃f+1 messages m' ∈ S"
+				
 				for _, mp := range vset {
 					// "∃<n,d',v'> ∈ m'.Q"
 					for _, emp := range mp.Qset {
@@ -781,7 +637,7 @@ nLoop:
 			quorum++
 		}
 
-		if quorum >= instance.intersectionQuorum() {
+		if quorum >= instance.intersectionQuorum() || (len(vset) <= (instance.N - instance.f) && instance.lastNewViewTimeout >= 8) {
 			// "then select the null request for number n"
 			msgList[n] = ""
 
@@ -789,6 +645,7 @@ nLoop:
 		}
 
 		logger.Warningf("Replica %d could not assign value to contents of seqNo %d, found only %d missing P entries", instance.id, n, quorum)
+		instance.showMsgList("assignSequenceNumbers",maxN,vset,msgList)
 		return nil
 	}
 
